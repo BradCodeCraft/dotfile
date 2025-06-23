99c5b6d07e11c1fc22f11cbb71e51b87d094039f
bradcodecraft <bradcodecraft@gmail.com>
2025-06-22 22:14:43 -0400

    Update autolocking feature
    This change the autolock dependency from xss-lock
    to xautolock to allow preventing locking screen

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm i3-wm i3status i3blocks xss-lock rofi mesa intel-ucode ly kitty neovim git xorg xorg-xinit xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-gnome kitty neovim firefox firefox-developer-edition chromium github-cli nodejs npm unzip pavucontrol stow brightnessctl xournalpp bluez bluez-utils blueman dunst libnotify lxappearance pacman-contrib acpi acpid papirus-icon-theme tmux ttf-jetbrains-mono-nerd
+sudo pacman -S --noconfirm i3-wm i3status i3blocks rofi mesa intel-ucode ly kitty neovim git xorg xorg-xinit xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-gnome firefox chromium github-cli nodejs npm unzip pavucontrol stow brightnessctl xournalpp bluez bluez-utils blueman dunst libnotify lxappearance pacman-contrib acpi acpid papirus-icon-theme tmux ttf-jetbrains-mono-nerd
+
+sudo yay -S xautolock
