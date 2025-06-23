fb41e24c2c0d06b5482cada91381acecac2b4982
bradcodecraft <bradcodecraft@gmail.com>
2025-06-22 22:37:15 -0400

    Update dunst config
    Change the behavior of volume and brightness notification
    (ignore history).

--- .local/bin/brightness.sh
+++ .local/bin/brightness.sh
@@ -4,4 +4,4 @@ CURRENT=$(brightnessctl g)
 MAX=$(brightnessctl m)
 BRIGHTNESS_LEVEL=$(expr $(expr $(expr $CURRENT \* 100) + $(expr $MAX - 1)) / $MAX)
 
-dunstify -h string:x-brightness "Brightness[$BRIGHTNESS_LEVEL%]" -h int:value:$BRIGHTNESS_LEVEL
+dunstify -a "changebrightness" -u low -r 9994 -h string:x-brightness "Brightness: $BRIGHTNESS_LEVEL%" -h int:value:$BRIGHTNESS_LEVEL
