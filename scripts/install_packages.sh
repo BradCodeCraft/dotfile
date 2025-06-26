14dbad2416c8cfb7ffe17a643f9033591ba3565c
bradcodecraft <bradcodecraft@gmail.com>
2025-06-25 23:49:20 -0400

    Add new package

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm i3-wm i3status i3blocks rofi mesa intel-ucode ly kitty neovim git xorg xorg-xinit xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-gnome firefox chromium github-cli nodejs npm unzip pavucontrol stow brightnessctl xournalpp bluez bluez-utils blueman dunst libnotify lxappearance pacman-contrib acpi acpid papirus-icon-theme tmux ttf-jetbrains-mono-nerd arandr
+sudo pacman -S --noconfirm i3-wm i3status i3blocks i3lock rofi mesa intel-ucode ly kitty neovim git xorg xorg-xinit xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-gnome firefox chromium github-cli nodejs npm unzip pavucontrol stow brightnessctl xournalpp bluez bluez-utils blueman dunst libnotify lxappearance pacman-contrib acpi acpid papirus-icon-theme tmux ttf-jetbrains-mono-nerd arandr
 
 sudo yay -S xautolock
