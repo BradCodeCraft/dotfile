f80621ca5748ff599a61f56ecf58ae8e07537db4
bradcodecraft <bradcodecraft@gmail.com>
2025-06-25 23:39:46 -0400

    Update scripts
    This will install yay first and setup service post installation.

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,14 +6,17 @@
 # | |___) \ V  V /| | | | | |
 # |_|____/ \_/\_/ |_| |_| |_|
 
-echo "Installing packages"
-. $HOME/dotfile/scripts/install_packages.sh
-
 echo "Installing yay"
 . $HOME/dotfile/scripts/install_yay.sh
+
+echo "Installing packages"
+. $HOME/dotfile/scripts/install_packages.sh
 
 echo "Setting up Git"
 . $HOME/dotfile/scripts/setup_git.sh
 
 echo "Setting up config"
 . $HOME/dotfile/scripts/setup_config.sh
+
+echo "Setting up service"
+. $HOME/dotfile/scripts/setup_service.sh
